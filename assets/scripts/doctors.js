e4aee593458f792c18f2e42446fbc1d3e79bedd1
DavidAjanaku <davidajanaku46163@gmail.com>
2023-12-29 21:52:34 +0100

    first commit

--- assets/scripts/doctors.js
+++ assets/scripts/doctors.js
@@ -18,18 +18,31 @@ document.addEventListener('DOMContentLoaded', function () {
 function filterByRating() {
   const selectedRating = document.getElementById('ratingFilter').value;
   const doctorCards = document.querySelectorAll('.doctor-card');
+  let foundDoctors = false;
 
   doctorCards.forEach(card => {
     const ratingElement = card.querySelector('.doctor-content p:last-child');
     const rating = ratingElement ? parseFloat(ratingElement.innerText.split(': ')[1]).toFixed(1) : 0;
 
-
     if (selectedRating === 'all' || rating >= parseFloat(selectedRating)) {
       card.style.display = 'flex';
+      foundDoctors = true;
     } else {
       card.style.display = 'none';
     }
   });
+
+  // Get the element where you want to display the message
+  const messageElement = document.getElementById('noDoctorsMessage');
+
+  if (!foundDoctors) {
+    // Display a message when no doctors match the selected rating
+    messageElement.innerHTML = 'No doctors with this rating.';
+  } else {
+    // Clear the message when doctors are found
+    messageElement.innerHTML = 'No doctors with this rating.';
+  }
 }
+
 
 
